78f8a4922158c114f791711a8d2130260a61de44
JoelHernadez343 <joel.programador@gmail.com>
2017-03-07 15:52:28 -0600

    Con particles HD

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {	
+	//=========== EFECTO PUSHPIN ============================
+	$('.target').pushpin({
+      		top: 0,
+      		bottom: 1000,
+      	offset: 0
+    });
+
+	$('.pushpin-demo-nav').each(function() {
+    	var $this = $(this);
+    	var $target = $('#' + $(this).attr('data-target'));
+    	$this.pushpin({
+      		top: $target.offset().top,
+      		bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    	});
+  	});
+    //Removes pushpin and pushpin classes
+ 	$('.tabs-wrapper .row').pushpin('remove');
+
+
+});
