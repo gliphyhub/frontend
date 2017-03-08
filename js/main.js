8a3c52e566755d057fde8cc5521c74399b54ab18
Roberto <rgg.correo@gmail.com>
2017-03-07 22:37:00 -0600

    Se arreglo el scroll y se inicio el contenido

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {	
-	//=========== EFECTO PUSHPIN ============================
+	//=========== EFECTO PUSHPIN ====================================
 	$('.target').pushpin({
       		top: 0,
       		bottom: 1000,
@@ -14,12 +14,26 @@ $(document).ready(function () {
       		bottom: $target.offset().top + $target.outerHeight() - $this.height()
     	});
   	});
-    //Removes pushpin and pushpin classes
- 	$('.tabs-wrapper .row').pushpin('remove');
-
-// ======== deslizar =========
- $(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
+  //======== deslizar ===============================================
+    $("#aboutus ul li:nth-child(1),#exp ul li:nth-child(2),#team ul li:nth-child(2)").click(function(e){
+        e.preventDefault()
+       var dest= $("#aboutg").offset().top;
+        $('html, body').animate( {scrollTop : dest}, 500);
+    });
+    $("#aboutg ul li:nth-child(1),#exp ul li:nth-child(1),#team ul li:nth-child(1)").click(function(e){
+        e.preventDefault()
+       var dest= $("#aboutus").offset().top;
+        $('html, body').animate( {scrollTop : dest}, 500);
+    });
+    $("#aboutus ul li:nth-child(2),#aboutg ul li:nth-child(2),#team ul li:nth-child(3)").click(function(e){
+        e.preventDefault()
+       var dest= $("#exp").offset().top;
+        $('html, body').animate( {scrollTop : dest}, 500);
+    });
+    $("#aboutus ul li:nth-child(3),#aboutg ul li:nth-child(3),#exp ul li:nth-child(3)").click(function(e){
+        e.preventDefault()
+       var dest= $("#team").offset().top;
+        $('html, body').animate( {scrollTop : dest}, 500);
+    });
 
 });
