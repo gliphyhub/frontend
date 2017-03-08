e8f244aecd9a2b339e3dc88feae13db9382f3986
Roberto <rgg.correo@gmail.com>
2017-03-08 17:20:48 -0600

    Se quito la experiencias

--- js/main.js
+++ js/main.js
@@ -15,22 +15,17 @@ $(document).ready(function () {
     	});
   	});
   //======== deslizar ===============================================
-    $("#aboutus ul li:nth-child(1),#exp ul li:nth-child(2),#team ul li:nth-child(2)").click(function(e){
+    $("#aboutus ul li:nth-child(1),#team ul li:nth-child(2)").click(function(e){
         e.preventDefault()
        var dest= $("#aboutg").offset().top;
         $('html, body').animate( {scrollTop : dest}, 500);
     });
-    $("#aboutg ul li:nth-child(1),#exp ul li:nth-child(1),#team ul li:nth-child(1)").click(function(e){
+    $("#aboutg ul li:nth-child(1),#team ul li:nth-child(1)").click(function(e){
         e.preventDefault()
        var dest= $("#aboutus").offset().top;
         $('html, body').animate( {scrollTop : dest}, 500);
     });
-    $("#aboutus ul li:nth-child(2),#aboutg ul li:nth-child(2),#team ul li:nth-child(3)").click(function(e){
-        e.preventDefault()
-       var dest= $("#exp").offset().top;
-        $('html, body').animate( {scrollTop : dest}, 500);
-    });
-    $("#aboutus ul li:nth-child(3),#aboutg ul li:nth-child(3),#exp ul li:nth-child(3)").click(function(e){
+    $("#aboutus ul li:nth-child(2),#aboutg ul li:nth-child(2)").click(function(e){
         e.preventDefault()
        var dest= $("#team").offset().top;
         $('html, body').animate( {scrollTop : dest}, 500);
