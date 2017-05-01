5cf317137f57eec615c0a1730ec26ed3fde99155
Roberto <rgg.correo@gmail.com>
2017-04-30 19:35:28 -0500

    Agregando animaciones simples y nueva propuesta de cards

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
        var dest= $("#team").offset().top;
         $('html, body').animate( {scrollTop : dest}, 500);
     });
-  //============evitar inciar sesion=====================
+  /*============evitar inciar sesion=====================
   $("form button").click(function (e) {
     e.preventDefault();
   });
@@ -48,7 +48,7 @@ $(document).ready(function () {
   });
    $(".collapsible-header").click(function () {
     $("#flecha_up_down").text("keyboard_arrow_down");
-   });
+   });*/
   // =============== CONTAR CARACTERES ====================
     $('input#input_text, textarea#textarea1').characterCounter();
   // =============== ComboBox =========================
@@ -99,31 +99,94 @@ $(document).ready(function () {
   $('#atras').click(function(){
     $('.carousel').carousel('prev');
   });
+
+  var menunavegaciónLargeVisible = true;
   $('#toggleMenu').click(function() {
-    $('#navegacion-no-oculto').toggleClass("ocultado");
-    $('#menu').toggleClass("hide-on-large-only");
+
+    if (menunavegaciónLargeVisible){
+      $('#navegacion-no-oculto').animate({width: '0px'},"fast");
+      $('header, main, .botones-flotantes').animate({paddingLeft: '0px'},"fast");
+      $('#location').animate({paddingLeft: '3px'});
+      $('#toggleMenu i').text("view_quilt");
+    }else{
+      $('#navegacion-no-oculto').animate({width: '240px'},"fast");
+      $('header, main, .botones-flotantes').animate({paddingLeft: '240px'},"fast");
+      $('#location').animate({paddingLeft: '0px'});
+      $('#toggleMenu i').text("view_stream");
+    }   
+
+    menunavegaciónLargeVisible = !menunavegaciónLargeVisible
+    $('#menu').toggleClass("hide-on-large-only");   
     $('header, main, .botones-flotantes').toggleClass("navegacion-espacio");
-    $('#toggleMenu').toggleClass("grey darken-1");
+    //$('#navegacion-no-oculto').toggleClass("ocultado");
   });
 
+
   $('#vista-simple').click(function () {
-    $('#contenido-comunicado-large, .v-large').addClass('ocultado');
+    //$('#contenido-comunicado-large).animate({opacity: '0.0'}, "slow");
+    //$('#contenido-comunicado-large, .v-large').addClass('ocultado'); //quitar el v-large
+    $('#contenido-comunicado-large').fadeOut(1);
+    //$('#contenido-large').animate({bottom:'100%'}, 400);
+
+
     $('.v-touch').removeClass("hide-on-large-only");
     $('#vista-simple').addClass('red white-bonis');
     $('#vista-tres-columnas').removeClass('red white-bonis');
-    $('#parte-izquierda').removeClass('l7');
-    $('#diversos, #cards-comunicados').addClass('l6');
-    $('#calendario-contenedor, #flitros-contenedor').addClass('l12');
 
+    
+    ///$('#parte-izquierda').animate({width: '58.3333333333%'},"slow");    
+    $('#parte-izquierda').removeClass('l7');
+    //$('#diversos, #cards-comunicados').addClass('l6');
+    //$('#calendario-contenedor, #flitros-contenedor').addClass('l12');
+
+    //usar ajazx
+    /*if ($('#cards-comunicados .row a').hasClass("urgente-card")){
+    }else{
+      $("#cards-comunicados .row a").removeClass("m6");
+      $("#cards-comunicados .row a").addClass("m12");
+      }*/
   });
 
   $('#vista-tres-columnas').click(function () {
-    $('#contenido-comunicado-large, .v-large').removeClass('ocultado');
+    //$('#contenido-comunicado-large, .v-large').removeClass('ocultado');//quitar el v-large
+      
+    //$('#contenido-large').removeClass("fixed");
+
     $('.v-touch').addClass("hide-on-large-only");
     $('#vista-tres-columnas').addClass('red white-bonis');
     $('#vista-simple').removeClass('red white-bonis');
+
+    
     $('#parte-izquierda').addClass('l7');
-    $('#diversos, #cards-comunicados').removeClass('l6');
-    $('#calendario-contenedor, #flitros-contenedor').removeClass('l12');
+    //$('#contenido-large').addClass("fixed");
+    //$('#diversos, #cards-comunicados').removeClass('l6');
+    //$('#calendario-contenedor, #flitros-contenedor').removeClass('l12');
+
+    //usar ajazx
+    /*if ($('#cards-comunicados .row a').hasClass("urgente-card")){
+     }else{
+      $("#cards-comunicados .row a").removeClass("m6");
+      $("#cards-comunicados .row a").addClass("m12");
+      }*/
+     $('#contenido-comunicado-large').fadeIn(1);
+     $('#contenido-large').animate({opacity: '0.0',}, 0);
+     $('#contenido-large').animate({opacity: '0.0',}, 900);
+     $('#contenido-large').animate({opacity: '1',}, 900);
   });
+
+  $('#cards-comunicados .row a:nth-child(1)').click(function (e){
+    e.preventDefault();
+    //$('#contenido-large').slideUp(1);
+    //$('#contenido-large').slideDown(1000);
+    $('#contenido-large .card-content').animate({opacity: '0.1'}, 1);
+    $('#contenido-large').animate({bottom:'100%'}, 100);    
+    $('#contenido-large').animate({bottom:'15px'}, 400);
+    $('#contenido-large .head-card').animate({width :'150px'}, 400);
+    $('#contenido-large .head-card').animate({width :'100%'}, 400);
+    $('#contenido-large .card-content').animate({opacity: '1'}, 1500);
+
+    
+   
+  });
+ 
 });
