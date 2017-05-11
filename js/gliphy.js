1bfbbd3a6ba58b77e289455d9bd232b59c8e8ae2
JoelHernadez343 <joel.programador@gmail.com>
2017-05-10 21:34:17 -0500

    Agregado el olvidé mi contraseña button

--- js/gliphy.js
+++ js/gliphy.js
@@ -1,35 +1,4 @@
 $(function() {
-	  //=========== EFECTO PUSHPIN ====================================
-	$('.target').pushpin({
-		top: 0,
-		bottom: 1000,
-		offset: 0
-	});
-	$('.pushpin-demo-nav').each(function() {
-		var $this = $(this);
-		var $target = $('#' + $(this).attr('data-target'));
-		//console.log ($target);
-		$this.pushpin({
-			top: $target.offset().top,
-			bottom: $target.offset().top + $target.outerHeight() - $this.height()
-		});
-	});
-	//======== deslizar ===============================================
-	$("#aboutus ul li:nth-child(1),#team ul li:nth-child(2)").click(function(e){
-		e.preventDefault()
-		var dest= $("#aboutg").offset().top;
-		$('html, body').animate( {scrollTop : dest}, 500);
-	});
-	$("#aboutg ul li:nth-child(1),#team ul li:nth-child(1)").click(function(e){
-		e.preventDefault()
-		var dest= $("#aboutus").offset().top;
-		$('html, body').animate( {scrollTop : dest}, 500);
-	});
-	$("#aboutus ul li:nth-child(2),#aboutg ul li:nth-child(2)").click(function(e){
-		e.preventDefault()
-		var dest= $("#team").offset().top;
-		$('html, body').animate( {scrollTop : dest}, 500);
-	});
 	  /*============evitar inciar sesion=====================
 	  $("form button").click(function (e) {
 	    e.preventDefault();
@@ -244,5 +213,28 @@ $(function() {
 
 	});	
 	
+
+	//======= Forget =========
+
+
+	$("#forget").on("click", function() {
+		$(".profile-card").removeClass("profileAnimated");
+		$(".profile-card").removeClass("anchuraExtraOff");
+		$(".profile-card").addClass("profileFinal");
+		$('.profile-card').addClass("anchuraExtra");
+		$(".profile-card .animate").addClass("animated");
+		$(".profile-card .animate").removeClass("animate");
+		$("#inicialForm, .logotipo-login").fadeOut();
+		$("#forgotten").delay(400).fadeIn(2000);
+		
+	});
+
+	$("#backLogin").on("click", function() {
+		$('.profile-card').removeClass("anchuraExtra");
+		$(".profile-card").addClass("anchoCambiado");
+		$(".profile-card").addClass("anchuraExtraOff");
+		$("#forgotten").fadeOut();
+		$("#inicialForm, .logotipo-login").delay(400).fadeIn(2000);
+	});
 });
 
